13c001747676ae2ad84f281c74974d2a05b85dc3
bastiwaterloo <sebastian.waterloo@xtheon.com>
2019-02-27 18:13:58 +0100

    Updates

---
+++ precache-manifest.812b1819985b2df735fb723bbe4179b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b5a3ffa982a86d76f725",
+    "url": "/deploy-test/static/js/runtime~main.b5a3ffa9.js"
+  },
+  {
+    "revision": "40da5aa97218d228ef32",
+    "url": "/deploy-test/static/js/main.40da5aa9.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/deploy-test/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "40da5aa97218d228ef32",
+    "url": "/deploy-test/static/css/main.84d012e6.chunk.css"
+  },
+  {
+    "revision": "94f10ec9e4f2593042da0a7503c01e12",
+    "url": "/deploy-test/index.html"
+  }
+];
